5f4916d7412706ba1a941469b7cf485fef2fe476
SimonGideon <simongideon918@gmail.com>
2023-01-05 01:51:30 +0300

    Help button and image

--- src/app.js
+++ src/app.js
@@ -4,9 +4,11 @@ import './styles/styles.css';
 import navRender from './modules/navbar.js';
 import hero from './modules/hero.js';
 import coreValues from './modules/coreValues.js';
-import upcommingEvents from './modules/upcomingEvent.js'
+import upcommingEvents from './modules/upcomingEvent.js';
+import help from './modules/help.js';
 
 hero();
 navRender();
 coreValues();
 upcommingEvents();
+help();
--- src/modules/help.js
+++ src/modules/help.js
@@ -1,13 +1,17 @@
-import help from '../images/help.jpg';
+import helpImg from '../images/help.jpg';
+
 const help = () => {
   const section = document.querySelector('section3');
-  let htnl = '';
+  let html = '';
   html += `
-  <div>
+  <div class="help">
     <h1>Help Someone Today</h1>
+    <img src="${helpImg}" alt="helpImg" class="helpImg"/>
     <p> Your kind donation will allow us to continue fighting for everyone's health, education, and financial security in your neighborhood and around the world.</p>
-  </div>
-  `
+    <button type='submit'>Donate Now<button>
+    </div>
+  `;
+  section.innerHTML = html;
 };
 
 export default help;
